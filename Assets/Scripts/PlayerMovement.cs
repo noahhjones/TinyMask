a73185fb6fa5780658d430493c48a975664ccbb2
Xrainer101 <146404942+Xrainer101@users.noreply.github.com>
2026-02-01 01:11:54 -0800

    Added animations
    Added animations for walking, climbing, and interacting
    Implementing walking animations and flipping
    Changed rigidbody from publicly set to private in code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     float horizontalMovement;
+    bool isFacingRight = true;
+
     public float moveSpeed = 5f;
 
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,15 +28,27 @@ public class PlayerMovement : MonoBehaviour
         if (PauseController.IsGamePaused)
         {
             rb.velocity = Vector2.zero;
-            //animator.SetBool("isWalking", false);
+            animator.SetBool("IsWalking", false);
             return;
         }
+
+        Flip();
+
         rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
-        //animator.SetBool("isWalking", rb.velocity.magnitude > 0);
+        animator.SetBool("IsWalking", rb.velocity.magnitude > 0);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    void Flip()
+    {
+        if (isFacingRight && horizontalMovement < 0 || !isFacingRight && horizontalMovement > 0)
+        {
+            isFacingRight = !isFacingRight;
+            sprite.flipX = !sprite.flipX;
+        }
     }
 }
